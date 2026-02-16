c58a7589510bbb2a6976a9a34c66681e66b89477
Swaminathan V <sv0829@srmist.edu.in>
2026-02-16 09:23:05 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App - UC1
+ * Prints OOPS to console
+ *
+ * @author Indukuri
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
