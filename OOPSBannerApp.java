af64889bd34528b8313feed033e438da30dd6020
Swaminathan V <sv0829@srmist.edu.in>
2026-02-17 11:27:01 +0530

    UC3: Print OOPS banner using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,54 @@
-/**
- * OOPS Banner App - UC2
- * Prints OOPS as a banner using asterisks
- *
- * @author Indukuri
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * * * * * * * * *");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *             *");
-        System.out.println(" *****   *****  *        ***** ");
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+        ));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+        ));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** "
+        ));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        ));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"
+        ));
+
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "
+        ));
     }
 }
