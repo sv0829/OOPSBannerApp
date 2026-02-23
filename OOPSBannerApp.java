fb5c640d3758e1f5147ced6f6d79a544ed048e32
Swaminathan V <sv0829@srmist.edu.in>
2026-02-23 11:27:38 +0530

    Added UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,19 @@
-/**
- * OOPS Banner App
- * UC4 - Print OOPS using Array & Loop
- *
- * @author Shyam
- * @version 1.0
- */
-public class OOPSBannerApp {
+class OOPSBanner {
 
     public static void main(String[] args) {
 
+        // Inline array initialization using String.join()
         String[] bannerLines = {
-            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*     *", "*      "),
-            String.join("   ", "*     *", "*     *", "*     *", "*      "),
-            String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*      ", "      *"),
-            String.join("   ", "*     *", "*     *", "*      ", "      *"),
-            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*       ", "      *"),
+            String.join("", "*     * ", "*     * ", "*       ", "      *"),
+            String.join("", " *****  ", " *****  ", "*       ", " *****")
         };
 
+        // Enhanced for-loop to display banner
         for (String line : bannerLines) {
             System.out.println(line);
         }
