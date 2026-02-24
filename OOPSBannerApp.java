98dc830cdb65501385f7912a65eb33a696ad9623
Swaminathan V <sv0829@srmist.edu.in>
2026-02-24 10:02:25 +0530

    UC6: Render OOPS banner using static helper functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,59 @@
-class OOPSBanner {
+/**
+ * OOPS Banner App
+ * UC6 - Render OOPS using Static Functions
+ *
+ * @author Shyam
+ * @version 1.0
+ */
+public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Inline array initialization using String.join()
-        String[] bannerLines = {
-            String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
-            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
-            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
-            String.join("", "*     * ", "*     * ", " *****  ", " ***** "),
-            String.join("", "*     * ", "*     * ", "*       ", "      *"),
-            String.join("", "*     * ", "*     * ", "*       ", "      *"),
-            String.join("", " *****  ", " *****  ", "*       ", " *****")
-        };
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-        // Enhanced for-loop to display banner
-        for (String line : bannerLines) {
-            System.out.println(line);
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(String.join("   ", O[i], O[i], P[i], S[i]));
         }
+    }
+
+    // Static method for letter O
+    static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Static method for letter P
+    static String[] getP() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Static method for letter S
+    static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
